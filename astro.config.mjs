291a4915a83869914bcc47e5d2bf2b6d5bf11001
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2025-08-19 15:25:20 +0200

    Refactor project structure and update team section
    - Changed project name in package.json from "website-2025" to "home".
    - Removed unused SVG files: favicon.svg, hero-motion.svg, member-1.svg, member-2.svg, member-3.svg.
    - Added new team member images: hansen_clint.jpg, romijnders_robbin.jpg, welzel_julius.jpg.
    - Deleted contact.astro page and its associated styles and scripts.
    - Updated index.astro to reflect changes in team section, including renaming "Research Staff" to "Team" and updating member roles and images.
    - Created new empty publications.astro page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://motionmetricslab.github.io',
+	site: 'https://motionmetricslab.com',
 	base: '/',
 	integrations: [mdx(), sitemap()],
 });
